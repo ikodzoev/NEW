d2f15ff2f674bf4de261fa54420a535149013152
ikodzoev <veozdok@gmail.com>
2022-08-31 17:24:22 +0500

    add Sem1Task0 Sem1Task1 Sem1Task3 Sem1Task5 Sem1Task7  Please enter the commit message for your changes. Lines starting

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,12 @@
+﻿//===============================================================
+//# 0 Напишите программу, которая на вход принимает 
+// число и выдаёт его квадрат (число, умноженное на самое себя).
+//================================================================
+string? inputLine = Console.ReadLine();
+if (inputLine != null) ;
+{
+    int inputNumber = int.Parse(inputLine);
+    int outNumber = inputNumber * inputNumber;
+    Console.WriteLine(outNumber);
+
+}
