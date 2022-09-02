1008101e79ed11945ab938057aa7d53931f9b0cc
ikodzoev <veozdok@gmail.com>
2022-09-02 12:51:16 +0500

    add homeworks:  Sem1Task2/ Sem1Task4/ Sem1Task6/ Sem1Task8/

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// =============================================================================================================
+// # 2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//==============================================================================================================
+Console.WriteLine("Введите два числа: ");
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null)
+
+{
+    int inputA = int.Parse(inputLineA);
+    int inputB = int.Parse(inputLineB);
+    if (inputA == inputB)
+        Console.WriteLine("Введённые числа равны");
+    else
+    {
+        if (inputA > inputB)
+        {
+            Console.WriteLine("Большее число: {0}", inputA);
+            Console.WriteLine("Меньшее число: {0}", inputB);
+        }
+        else
+        {
+            Console.WriteLine("Большее число: {0}", inputB);
+            Console.WriteLine("Меньшее число: {0}", inputA);
+
+        }
+    }
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿// ==================================================================================================
+// # 4  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//===================================================================================================
+Console.WriteLine("Введите три числа: ");
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+
+{
+    int inputA = int.Parse(inputLineA);
+    int inputB = int.Parse(inputLineB);
+    int inputC = int.Parse(inputLineC);
+
+    if (inputA > inputB)
+        if (inputA > inputC)
+        {
+            Console.WriteLine("Большее число: {0}", inputA);
+        }
+        else
+        {
+            Console.WriteLine("Большее число: {0}", inputC);
+        }
+    else
+    {
+        if (inputB > inputC)
+        {
+            Console.WriteLine("Большее число: {0}", inputB);
+        }
+        else
+        {
+            Console.WriteLine("Большее число: {0}", inputC);
+        }
+    }
+}
