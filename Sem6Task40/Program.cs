b25f8431823ddd9db0b3ab153511f966814454f8
ikodzoev <veozdok@gmail.com>
2022-09-18 15:52:32 +0500

    Добавлены задачи семинара  39, 40, 41, 42, 43, 44, 45

---
+++ Sem6Task40/Program.cs
@@ -0,0 +1,55 @@
+﻿// // Метод считывания данных пользователя
+// int ReadData(string line)
+// {
+//     // Выводим сообщение
+//     Console.Write(line);
+//     // Считываем число
+//     int number = int.Parse(Console.ReadLine() ?? "0");
+//     // Возвращаем значение
+//     return number;
+// }
+
+// // Метод, принимает строку, выводит в консоль
+// void PrintResult(string line)
+// {
+//     Console.WriteLine(line);
+// }
+
+// bool TriangleTest(int a, int b, int c)
+// {
+//     return (a + b > c) && (a + c > b) && (b + c > a);
+// }
+
+// int a = ReadData("Введите длину стороны a: ");
+// int b = ReadData("Введите длину стороны b: ");
+// int c = ReadData("Введите длину стороны c: ");
+
+// PrintResult(TriangleTest(a, b, c) ? "Такой треугольник может быть" : "Такого треугольника не может быть");
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Печать результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+bool TrglTest(int a, int b, int c)
+{
+    if (a + b < c && a + c < b && b + c < a && a > 0 && b > 0 && c > 0) return true;
+    return false;
+}
+
+int a = ReadData("Введите сторону треугольника а: ");
+int b = ReadData("Введите сторону треугольника b: ");
+int c = ReadData("Введите сторону треугольника c: ");
+
+PrintResult(TrglTest(a,b,c)?"Такой треугольник существует":"Такого треугольника не существует");
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,42 @@
+﻿//===============================================================================================
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//===============================================================================================
+int ReadData(string line)
+{
+    // Выводим сообщение;
+    Console.WriteLine(line);
+    // Считываем число;
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение;
+    return number;
+}
+
+void PrintData(string line) // вывод результата вычисления;
+{
+    Console.WriteLine(line);
+}
+
+double[] FindPoint(double b1, double k1, double b2, double k2) // вычисляем точку пересечения прямых;
+{
+    double[] outArray = new double[2];
+    double xLine = 0;
+    double yLine = 0;
+    if (b1 == b2 & k1 == k2){Console.WriteLine("Прямые совпадают");}
+    else
+    {
+        if (k1 == k2) {Console.WriteLine("Прямые параллельны");}
+        else{xLine = (b2 - b1) / (k1 - k2); yLine = k1 * xLine + b1;}
+    }
+    outArray[0] = xLine;
+    outArray[1] = yLine;
+    return outArray;
+}
+double b1 = ReadData("Введите значение b1: ");
+double k1 = ReadData("Введите значение k1: ");
+double b2 = ReadData("Введите значение b2: ");
+double k2 = ReadData("Введите значение k2: ");
+
+double[] result = FindPoint(b1, k1, b2, k2);
+
+PrintData("Точка пересечения двух прямых: " + " (" + result[0] + " ; " + result[1] + ")");
