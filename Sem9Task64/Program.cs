928bc7c24ef51724f4f2a63fbb0e2b75d8a77759
ikodzoev <veozdok@gmail.com>
2022-09-28 17:12:46 +0500

    Добавлены задачи №№ 64, 66

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,31 @@
+﻿// ============================================================================
+// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// ============================================================================
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine("Натуральные числа в промежутке от N до 1 и обратно: ");
+    Console.WriteLine(line);
+}
+
+// Рекурсивный метод
+string RecurLine(int N) 
+{
+    if (N == 0) return " ";
+    string res = N + " | " + RecurLine(N - 1) + " | " + N;
+    return res;
+}
+
+int N = ReadData("Введите число N: ");
+string result = RecurLine(N);
+PrintResult(result);
---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,36 @@
+﻿// ==================================================================
+// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// ==================================================================
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string prefix, int data)
+{
+    if (data == 0) { Console.WriteLine("Число N должно быть больше или равно M!"); }
+    else { Console.WriteLine("Сумма натуральных чисел в промежутке от M до N: " + data); }
+}
+
+// Рекурсивный метод;
+int RecurSum(int M, int N)
+{
+    if (N >= M)
+    {
+        if (M == N) { return M; }
+        else { return M + RecurSum(M + 1, N); }
+    }
+    else return 0;
+}
+
+int M = ReadData("Введите число M: ");
+int N = ReadData("Введите число N: ");
+
+int result = RecurSum(M, N);
+PrintResult(" ", +result);
